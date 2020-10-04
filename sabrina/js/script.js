1ee0ad4e5ecbc2a16d0c0711be5827da88163050
Sabrina Mares <sabrsgomes@gmail.com>
2020-10-04 17:22:41 -0300

    script: to do list

---
+++ sabrina/js/script.js
@@ -0,0 +1,69 @@
+const adicionar = document.getElementById('todoSubmit');
+const input = document.getElementById('todoInput');
+const option = document.getElementById('tag');
+const marcar = document.getElementById('todoMarkAll');
+const limpar = document.getElementById('todoRemoveAll');
+const div = document.querySelector('.todo__container');
+let alerta = document.createElement('h2');
+const formulario = document.getElementById('todoForm')
+
+adicionar.addEventListener('click', montarLista);
+function montarLista(e) {
+    e.preventDefault();
+
+    const lista = document.getElementById('todoList');
+    const itemLista = document.createElement('li');
+    const textoOption = document.createElement('p');
+    const textoInput = document.createElement('p');
+    const botaoDeletar = document.createElement('button');
+
+    if (input.value === '') {
+        alerta.textContent = 'Insira uma tarefa no campo abaixo.';
+        alerta.style.textAlign = 'center'
+        alerta.style.color = 'purple'
+        alerta.style.borderBottom = 'solid'
+        alerta.style.borderColor = 'purple'
+
+        div.appendChild(alerta);
+        alerta = div.insertBefore(alerta, formulario)
+    }
+
+    else {
+        alerta.textContent = null
+        alerta.style.border = 'none'
+
+        lista.appendChild(itemLista)
+        itemLista.appendChild(textoOption)
+        itemLista.appendChild(textoInput)
+        itemLista.appendChild(botaoDeletar)
+
+        itemLista.classList.add('todo__task-list')
+
+        textoOption.textContent = `${option.value}`
+        textoInput.textContent = `${input.value}`
+        botaoDeletar.textContent = 'X';
+        botaoDeletar.style.cursor = 'pointer'
+        botaoDeletar.style.backgroundColor = 'purple'
+        botaoDeletar.style.color = 'white'
+
+        botaoDeletar.addEventListener('click', function () {
+            lista.removeChild(itemLista)
+        })
+
+        input.value = null
+
+        marcar.addEventListener('click', clicou)
+        itemLista.addEventListener('click', clicou)
+        function clicou(){
+            textoOption.classList.toggle('checked');
+            textoInput.classList.toggle('checked');
+        }
+
+        limpar.addEventListener('click', function(){
+            lista.removeChild(itemLista)
+            console.log(itemLista)
+        })
+
+        
+    }
+}
